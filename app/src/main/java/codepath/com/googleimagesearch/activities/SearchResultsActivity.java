dfed4ff588cd02e2dee420ecd30b218d1ee261f0
Lori Lee <leelorik@gmail.com>
2015-02-12 21:40:17 -0800

    Replaced ActionBar with Toolbar; Added SearchView to Toolbar; Added GridView

--- app/src/main/java/codepath/com/googleimagesearch/activities/SearchResultsActivity.java
+++ app/src/main/java/codepath/com/googleimagesearch/activities/SearchResultsActivity.java
@@ -1,9 +1,14 @@
-package codepath.com.googleimagesearch;
+package codepath.com.googleimagesearch.activities;
 
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.SearchView;
+
+import codepath.com.googleimagesearch.R;
 
 
 public class SearchResultsActivity extends ActionBarActivity {
@@ -12,6 +17,8 @@ public class SearchResultsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
 
@@ -19,7 +26,20 @@ public class SearchResultsActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_search_results, menu);
-        return true;
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
